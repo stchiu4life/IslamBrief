b34a2f8d56544b073d00acad9b250e88d106ad5f
Ibrahim Kyuu <stchiu4life@gmail.com>
2017-08-12 18:23:22 +0800

    Initial Commit

---
+++ IslamBrief/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IslamBrief
+//
+//  Created by Ibrahim Kyuu on 2017/8/12.
+//  Copyright © 2017年 Ibrahim Kyuu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
